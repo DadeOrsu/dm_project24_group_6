1acbd51bcb95b262aa42c6cc036c2319f3f435cf
MirkoDangelo <m.dangelo20@studenti.unipi.it>
2024-10-04 22:36:18 +0200

    first raw analysis

--- src/task1_data_understanding/utils.py
+++ src/task1_data_understanding/utils.py
@@ -1,1 +1,17 @@
 #utils for task 1
+import matplotlib.pyplot as plt
+def plot_missing_values_barplot(missing_values_df):
+    values=missing_values_df[missing_values_df['missing values %']>0]['missing values %']
+    values['all other values']=0
+    bars=values.sort_values(ascending=False).plot(kind='bar',figsize=(15,10),ylim=[0,100])
+    plt.title('missing values %')
+    plt.xlabel('features')
+    plt.ylabel('missing values %')
+    plt.xticks(rotation=0, ha='center')
+    # adding percentage values above bars
+    for bar in bars.patches:
+        h=bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, 
+                h, 
+                str(h), 
+                ha='center', va='bottom')
