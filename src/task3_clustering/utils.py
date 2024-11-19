4764cd334f5b62c02d6e986a82d9bc0e4bfd6e4c
Mirko Michele D'angelo <41843282+mirdan08@users.noreply.github.com>
2024-11-19 19:12:24 +0100

    added decade segmentation on DBSCAN

--- src/task3_clustering/utils.py
+++ src/task3_clustering/utils.py
@@ -4,26 +4,32 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.metrics import silhouette_score
 import itertools as it
+import numpy as np
 #utils for task 3
 def run_dbscan(min_pts_values,eps_values,metric,clustering_data):
     results=pd.DataFrame()
     for idx,(eps,metric,min_pts) in enumerate(it.product(eps_values,metric,min_pts_values)):
-        db_scan=DBSCAN(
+        print(f"starting {idx}\n",{
+            'index':idx,
+            'eps':eps,
+            'metric':metric,
+            'min_samples':min_pts,
+        })
+        labels=DBSCAN(
             eps=eps,
             min_samples=min_pts,
             metric=metric,
             n_jobs=-1
-        ).fit(clustering_data)
+        ).fit_predict(clustering_data)
+        print(np.unique(labels,return_counts=True))
         #NOTE: the noisy labels are NOT taken into account 
-        results=pd.concat([
-            results,
-            {
+        new_conf={
                 'index':idx,
                 'eps':eps,
                 'metric':metric,
                 'min_samples':min_pts,
-                'silhoutte_score':silhouette_score(clustering_data,db_scan.labels_)
+                'silhoutte_score':silhouette_score(clustering_data,labels)
             }
-        ])
-        print(results[-1])
+        results=pd.concat([results,new_conf])
+        print("finished\n",results[-1])
     return results
