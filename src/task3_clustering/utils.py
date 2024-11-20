a9a8259943f36492d6fbb31a732e948407226c54
mirdan08 <mirdan0825@gmail.com>
2024-11-21 00:07:20 +0100

    finished db scan decade clustering esetup

--- src/task3_clustering/utils.py
+++ src/task3_clustering/utils.py
@@ -6,30 +6,23 @@ from sklearn.metrics import silhouette_score
 import itertools as it
 import numpy as np
 #utils for task 3
-def run_dbscan(min_pts_values,eps_values,metric,clustering_data):
-    results=pd.DataFrame()
+def run_dbscan(min_pts_values,eps_values,metric,clustering_data,precomputed_distances):
+    results=pd.DataFrame([{}])
     for idx,(eps,metric,min_pts) in enumerate(it.product(eps_values,metric,min_pts_values)):
-        print(f"starting {idx}\n",{
-            'index':idx,
-            'eps':eps,
-            'metric':metric,
-            'min_samples':min_pts,
-        })
         labels=DBSCAN(
             eps=eps,
             min_samples=min_pts,
-            metric=metric,
+            metric='precomputed',
             n_jobs=-1
-        ).fit_predict(clustering_data)
-        print(np.unique(labels,return_counts=True))
+        ).fit_predict(precomputed_distances)
         #NOTE: the noisy labels are NOT taken into account 
-        new_conf={
-                'index':idx,
+        new_conf=pd.DataFrame([{
+                'group_index':idx,
                 'eps':eps,
                 'metric':metric,
                 'min_samples':min_pts,
                 'silhoutte_score':silhouette_score(clustering_data,labels)
-            }
+            }])
         results=pd.concat([results,new_conf])
-        print("finished\n",results[-1])
+        print("finished\n",results.iloc[-1])
     return results
